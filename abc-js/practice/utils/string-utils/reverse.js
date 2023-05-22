a2e996f0443ad1adfd8b8658dae3a0fe714da23f
ZanshinKurs <131344637+ZanshinKurs@users.noreply.github.com>
2023-05-22 12:51:42 +0500

    Revert "My first praktika tarck"

--- abc-js/practice/utils/string-utils/reverse.js
+++ abc-js/practice/utils/string-utils/reverse.js
@@ -1,9 +1,3 @@
-import { len } from './len.js';
 /** Возвращает "развернутую" копию text */
 export function reverse(text) {
-    let reverseText = '';
-    for(let i = len(text)-1; i >= 0; i--){
-        reverseText += text[i];
-    }
-    return reverseText
 }
